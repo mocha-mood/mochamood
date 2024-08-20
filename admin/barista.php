6805e4bd3c307862e060380fa6379b3c66e944c9
tarahere <mtaraalleah@gmail.com>
2024-08-20 15:08:53 +0300

    removed order h1 title

--- admin/barista.php
+++ admin/barista.php
@@ -16,7 +16,7 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Luckiest+Guy&display=swap" rel="stylesheet"> 
     <link rel="stylesheet" href="admin.css">
-    <title>Barista Page</title>
+    <title>Orders</title>
 </head>
 <body>
 
@@ -46,8 +46,6 @@
   </header>
 
         <script src="menutoggle.js"></script>
-
-      <h1 id="title">Orders</h1>
 
       <div class="containertime">
     <div class="time">
